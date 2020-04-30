976460971d63e44f864d932be70be1b7d051a287
Eric <eric.silva@nsprevention.com.br>
2020-04-30 03:26:00 -0300

    Criada Landing Page.

--- pypro/urls.py
+++ pypro/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from base import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', include('base.urls')),
 ]
 
 if settings.DEBUG:
